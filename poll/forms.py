2d25eccdf4757e59b303e52519ea7c428b5c00d4
thuyphuocthinh <104757603+thuyphuocthinh@users.noreply.github.com>
2024-08-26 20:14:31 +0700

    finished project

---
+++ poll/forms.py
@@ -0,0 +1,8 @@
+# forms.py
+from django.forms import ModelForm
+from .models import Poll
+
+class CreatePollForm(ModelForm):
+    class Meta:
+        model = Poll
+        fields = ['question', 'option_one', 'option_two', 'option_three']
